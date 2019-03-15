f6eb9a95eb4a4bbd4339af2c93f196dde8f9f0ef
Anita Virag <ik.virag@gmail.com>
2019-03-15 16:53:28 +0100

    create dummy data for users and posts table

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-### DB
-* php artisan make:model Post -m
-* php artisan migrate
+### Data
+```
+$ php artisan make:model Post -m
+$ php artisan migrate
+$ php artisan make:seed PostsTableSeeder
+$ php artisan make:seed UsersTableSeeder
+$ php artisan tinker
+>>> $faker = Faker\Factory::create();
+>>> $faker->word
+>>> $faker->sentence
+>>> $faker->sentences
+>>> $faker->sentences(6)
+>>> q
+php artisan db:seed
+```
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // resets the users table
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('users')->truncate();
+
+        // generate 3 users/authors
+        DB::table('users')->insert([
+            [
+                'name' => "John Doe",
+                'email' => "johndoe@test.com",
+                'password' => bcrypt('secret')
+            ],            [
+                'name' => "Jane Doe",
+                'email' => "janedoe@test.com",
+                'password' => bcrypt('secret')
+            ],            [
+                'name' => "Leon Norman ",
+                'email' => "leonnorman@test.com",
+                'password' => bcrypt('secret')
+            ]
+        ]);
+    }
+}
