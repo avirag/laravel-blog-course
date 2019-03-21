6106d3ef2d04108c1adc4a7e521aa76e833a98e4
Anita Virag <ik.virag@gmail.com>
2019-03-21 22:27:28 +0100

    filter posts by publication status

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -25,18 +25,44 @@ class BlogController extends BackendController
      */
     public function index(Request $request)
     {
+        $onlyTrashed = false;
         $status = $request->get('status');
         if($status && $status === 'trash') {
             $posts = Post::onlyTrashed()->with('category', 'author')->latest()->paginate($this->limit);
             $postCount = Post::onlyTrashed()->count();
             $onlyTrashed = true;
+        } elseif($status === 'published') {
+            $posts = Post::published()->with('category', 'author')->latest()->paginate($this->limit);
+            $postCount = Post::count();
+
+        } elseif ($status === 'scheduled') {
+            $posts = Post::scheduled()->with('category', 'author')->latest()->paginate($this->limit);
+            $postCount = Post::count();
+
+        } elseif ($status === 'draft') {
+            $posts = Post::draft()->with('category', 'author')->latest()->paginate($this->limit);
+            $postCount = Post::count();
+
         } else {
             $posts = Post::with('category', 'author')->latest()->paginate($this->limit);
             $postCount = Post::count();
-            $onlyTrashed = false;
+
         }
 
-        return view('backend.blog.index', compact('posts', 'postCount', 'onlyTrashed'));
+        $statusList = $this->statusList();
+
+        return view('backend.blog.index', compact('posts', 'postCount', 'onlyTrashed', 'statusList'));
+    }
+
+    private function statusList()
+    {
+        return [
+          'all' => Post::count(),
+          'published' => Post::published()->count(),
+          'scheduled' => Post::scheduled()->count(),
+          'draft' => Post::draft()->count(),
+          'trash' => Post::onlyTrashed()->count()
+        ];
     }
 
     /**
@@ -123,8 +149,13 @@ class BlogController extends BackendController
     public function update(PostRequest $request, $id)
     {
         $post = Post::findOrFail($id);
+        $oldImage = $post->image;
         $data = $this->handleRequest($request);
         $post->update($data);
+
+        if($oldImage !== $post->image) {
+            $this->removeImage($oldImage);
+        }
 
         return redirect('backend/blog')->with('message', 'Your post was updated successfully!');
     }
@@ -143,7 +174,11 @@ class BlogController extends BackendController
 
     public function forceDestroy($id)
     {
-        Post::withTrashed()->findOrFail($id)->forceDelete();
+        $post = Post::withTrashed()->findOrFail($id);
+        $post->forceDelete();
+
+        $this->removeImage($post->image);
+
         return redirect('backend/blog?status=trash')->with('message', 'Your post has been deleted successfully!');
     }
 
@@ -152,6 +187,24 @@ class BlogController extends BackendController
         $post = Post::withTrashed()->findOrFail($id);
         $post->restore();
 
-        return redirect('backend/blog')->with('message', 'Your post has been moved from the Trash.');
+        return redirect()->back()->with('message', 'Your post has been moved from the Trash.');
+    }
+
+    private function removeImage($image)
+    {
+        if(!empty($image)) {
+            $imagePath = $this->uploadPath . '/' .$image;
+            $ext = substr(strrchr($image, '.'), 1);
+            $thumbnail = str_replace(".{$ext}", "_thumb.{$ext}", $image);
+            $thumbnailPath = $this->uploadPath . '/' .$thumbnail;
+
+            if(file_exists($imagePath)) {
+                unlink($imagePath);
+            }
+
+            if(file_exists($thumbnailPath)) {
+                unlink($thumbnailPath);
+            }
+        }
     }
 }
--- resources/views/backend/blog/index.blade.php
+++ resources/views/backend/blog/index.blade.php
@@ -28,8 +28,15 @@
             </div>
 
             <div class="pull-right" style="padding: 7px 0;">
-              <a href="?status=all">All</a> |
-              <a href="?status=trash">Trash</a>
+              <?php $links = [] ?>
+              @foreach($statusList as $key => $value)
+                @if($value)
+                  <?php $selected = Request::get('status') == $key ? 'selected-status' : '' ?>
+                  <?php $links[] = "<a class=\"{$selected}\" href=\"?status={$key}\">" . ucwords($key) . "({$value})</a>" ?>
+                @endif
+              @endforeach
+
+              {!! implode(' | ', $links) !!}
             </div>
           </div>
           <div class="box-body ">
@@ -51,7 +58,7 @@
 
           <div class="box-footer clearfix">
             <div class="pull-left">
-                {{ $posts->links() }}
+                {{ $posts->appends(Request::query())->render() }}
             </div>
 
             <div class="pull-right">
