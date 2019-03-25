81730662761f3c30b17c44765114a2b9468309c4
Anita Virag <ik.virag@gmail.com>
2019-03-25 13:17:38 +0100

    additional link to show only the current user's post

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -43,25 +43,30 @@ class BlogController extends BackendController
             $posts = Post::draft()->with('category', 'author')->latest()->paginate($this->limit);
             $postCount = Post::count();
 
+        } elseif ($status === 'own') {
+            $posts = $request->user()->posts()->with('category', 'author')->latest()->paginate($this->limit);
+            $postCount =$request->user()->posts()->count();
+
         } else {
             $posts = Post::with('category', 'author')->latest()->paginate($this->limit);
             $postCount = Post::count();
 
         }
 
-        $statusList = $this->statusList();
+        $statusList = $this->statusList($request);
 
         return view('backend.blog.index', compact('posts', 'postCount', 'onlyTrashed', 'statusList'));
     }
 
-    private function statusList()
+    private function statusList($request)
     {
         return [
-          'all' => Post::count(),
-          'published' => Post::published()->count(),
-          'scheduled' => Post::scheduled()->count(),
-          'draft' => Post::draft()->count(),
-          'trash' => Post::onlyTrashed()->count()
+            'own' => $request->user()->posts()->count(),
+            'all' => Post::count(),
+            'published' => Post::published()->count(),
+            'scheduled' => Post::scheduled()->count(),
+            'draft' => Post::draft()->count(),
+            'trash' => Post::onlyTrashed()->count()
         ];
     }
 
